873d2d4216ba33add3c8b9a4dc1f114470dee9dc
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2024-03-10 13:23:23 -0300

    Ajustando Resposividade da sessão sobre nos e aplicando animação na header

--- app-web/site/public/js/index.js
+++ app-web/site/public/js/index.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let elementoHeader = document.getElementById('header'); 
-    let quantidadePixelsDoTopo = 10; 
+document.addEventListener('DOMContentLoaded', function () {
+    let elementoHeader = document.getElementById('header');
+    let quantidadePixelsDoTopo = 80;
     window.addEventListener('scroll', function () { // Escuta se a tela vai scrola ou não
         if (window.scrollY > quantidadePixelsDoTopo) {
-            elementoHeader.classList.remove('changeStyle');
-        } else {
             elementoHeader.classList.add('changeStyle');
+        } else {
+            elementoHeader.classList.remove('changeStyle');
         }
     });
 });
