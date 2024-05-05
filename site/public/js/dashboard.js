74ac2a024de1faf862789f569f7843e896ebd846
JonathanCarvalho039 <jonathan.carvalho@sptech.school>
2024-05-05 14:51:15 -0300

    Criando tela de politicas de bloqueios

--- site/public/js/dashboard.js
+++ site/public/js/dashboard.js
@@ -115,11 +115,11 @@ function atualizar_maquina_tempo_real(
           status.innerHTML = 'Desligado'
           pc.style.color = 'black'
           pc.style.animation = 'none'
-          // Define todos os indicadores como inativos (cor cinza)
-          ;[bolinha_cpu, bolinha_ram, bolinha_disco].forEach(bolinha => {
-            bolinha.style.background = '#d2d2d2'
-            bolinha.style.animation = 'none'
-          })
+            // Define todos os indicadores como inativos (cor cinza)
+            ;[bolinha_cpu, bolinha_ram, bolinha_disco].forEach(bolinha => {
+              bolinha.style.background = '#d2d2d2'
+              bolinha.style.animation = 'none'
+            })
         }
       })
     })
@@ -338,7 +338,44 @@ function deletarMaquina(id_maquina) {
 let intervalo
 let id_maquina_pesquisa
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  let switchFlat1 = document.getElementById('switch-flat1');
+
+  if (switchFlat1) {
+    switchFlat1.addEventListener('change', function (event) {
+      let grafico_geral = document.getElementById('grafico_geral');
+      if (!event.target.checked) {
+        if (grafico_geral) {
+          setTimeout(function () {
+            grafico_geral.style.display = 'flex';
+          }, 500);
+        }
+      }else{
+        setTimeout(function () {
+          grafico_geral.style.display = 'none';
+        }, 500);
+      }
+    });
+  }
+});
+
+
+
+
 function atualizar_grafico_tempo_real(id_maquina) {
+
+
+
+  // Primeiro, selecione o elemento do botão usando seu ID
+  let switchFlat1 = document.getElementById('switch-flat1');
+
+  switchFlat1.checked = true
+
+  let grafico_geral = document.getElementById('grafico_geral')
+  grafico_geral.style.display = 'none'
+
+
   let dadoGraficoCpu = [
     null,
     null,
@@ -463,8 +500,7 @@ function atualizar_grafico_tempo_real(id_maquina) {
     null,
     null
   ]
-  let dadosGraficoDisco = [0, 0]
-
+  let dadosGraficoDisco = [0, 0];
   clearInterval(intervalo)
 
   intervalo = setInterval(() => {
@@ -485,7 +521,7 @@ function atualizar_grafico_tempo_real(id_maquina) {
               dadoGraficoRam.shift()
               dadoGraficoRam.push(
                 (informacoes[0].ram_ocupada_gb / informacoes[0].ram_total_gb) *
-                  100
+                100
               )
               myChartRam.data.datasets[0].data = dadoGraficoRam
               myChartRam.update()
@@ -775,192 +811,3 @@ function ocultarAcao(id) {
   }
 }
 
-const label = [
-  '0s',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  ' ',
-  '60s'
-]
-
-const label_hist = ['08 ás 10', '10 ás 12', '12 ás 14', '14 ás 16', '16 ás 18']
-
-const data = {
-  labels: label,
-  datasets: [
-    {
-      label: 'My First Dataset',
-      fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-      hoverOffset: 4,
-      pointRadius: 0,
-      backgroundColorColor: 'black',
-      borderWidth: 1.5
-    }
-  ]
-}
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {
-    plugins: {
-      legend: {
-        display: false
-      }
-    },
-    scales: {
-      y: {
-        max: 100,
-        min: 0
-      }
-    },
-    elements: {
-      line: {
-        tension: 0
-      }
-    },
-    // Definindo a cor de fundo do gráfico
-    backgroundColor: 'rgba(0, 0, 0, 0.1)' // Ajuste o último valor para mudar a transparência (0 é completamente transparente, 1 é completamente opaco)
-  }
-}
-
-const data1 = {
-  labels: label,
-  datasets: [
-    {
-      label: 'Uso de Memória RAM',
-      fill: true,
-      backgroundColor: 'rgba(000, 99, 132, 0.5)',
-      borderColor: 'rgba(000, 99, 132, 1)',
-      borderWidth: 1,
-      pointRadius: 0, // Definindo o tamanho do ponto como zero
-      hoverOffset: 4,
-      borderWidth: 1.5
-    }
-  ]
-}
-
-const config1 = {
-  type: 'line',
-  data: data1,
-  options: {
-    plugins: {
-      legend: {
-        display: false
-      }
-    },
-    scales: {
-      y: {
-        max: 100,
-        min: 0
-      }
-    },
-    elements: {
-      line: {
-        tension: 0
-      }
-    }
-  }
-}
-const data3 = {
-  labels: ['CPU', 'Ram', 'Disco'],
-  datasets: [
-    {
-      label: 'Quantidade de ',
-      fill: true,
-      backgroundColor: 'rgba(000, 99, 132, 0.5)',
-      borderColor: 'rgba(000, 99, 132, 1)'
-    }
-  ]
-}
-
-const config3 = {
-  type: 'bar',
-  data: data3,
-  options: {
-    plugins: {
-      legend: {
-        display: false
-      }
-    },
-    elements: {
-      line: {
-        tension: 0
-      }
-    }
-  }
-}
-const data2 = {
-  labels: ['Livre', 'Ocupado'],
-  datasets: [
-    {
-      label: 'My First Dataset',
-      data: [],
-      backgroundColor: ['green', 'red'],
-      hoverOffset: 4
-    }
-  ]
-}
-const config2 = {
-  type: 'pie',
-  data: data2
-}
---
+++ site/public/js/graficosDashboard.js
@@ -0,0 +1,212 @@
+const label = [
+  '0s',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  ' ',
+  '60s'
+]
+
+const label_hist = ['08 ás 10', '10 ás 12', '12 ás 14', '14 ás 16', '16 ás 18']
+
+const data = {
+  labels: label,
+  datasets: [
+    {
+      label: 'My First Dataset',
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+      hoverOffset: 4,
+      pointRadius: 0,
+      backgroundColorColor: 'black',
+      borderWidth: 1.5
+    }
+  ]
+}
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    scales: {
+      y: {
+        max: 100,
+        min: 0
+      }
+    },
+    elements: {
+      line: {
+        tension: 0
+      }
+    },
+    // Definindo a cor de fundo do gráfico
+    backgroundColor: 'rgba(0, 0, 0, 0.1)' // Ajuste o último valor para mudar a transparência (0 é completamente transparente, 1 é completamente opaco)
+  }
+}
+
+const data1 = {
+  labels: label,
+  datasets: [
+    {
+      label: 'Uso de Memória RAM',
+      fill: true,
+      backgroundColor: 'rgba(000, 99, 132, 0.5)',
+      borderColor: 'rgba(000, 99, 132, 1)',
+      borderWidth: 1,
+      pointRadius: 0, // Definindo o tamanho do ponto como zero
+      hoverOffset: 4,
+      borderWidth: 1.5
+    }
+  ]
+}
+
+const config1 = {
+  type: 'line',
+  data: data1,
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    scales: {
+      y: {
+        max: 100,
+        min: 0
+      }
+    },
+    elements: {
+      line: {
+        tension: 0
+      }
+    }
+  }
+}
+
+const data3 = {
+  labels: ['Redes Sociais', 'Streaming', 'Notícia', 'Jogos', 'Comunicação'],
+  datasets: [
+    {
+      data: [48, 12, 20, 16, 24],
+      label: 'Quantidade de ',
+      fill: true,
+      backgroundColor: '#2e4959',
+      borderColor: 'black'
+    }
+  ]
+}
+
+const config3 = {
+  type: 'bar',
+  data: data3,
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: 'Volume de encerrado no dia',
+          font: {
+            size: 13
+          }
+        }
+      },
+      x:{
+        title: {
+          display: true,
+          text: 'Categorias',
+          font: {
+            size: 13
+          }
+        }
+      }
+    },
+    elements: {
+      line: {
+        tension: 0
+      }
+    }
+  }
+};
+
+const data2 = {
+  labels: ['Livre', 'Ocupado'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [],
+      backgroundColor: ['green', 'red'],
+      hoverOffset: 4
+    }
+  ]
+}
+const config2 = {
+  type: 'pie',
+  data: data2
+}
