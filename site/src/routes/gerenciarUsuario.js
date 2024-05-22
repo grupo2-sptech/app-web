aa90327eb5f9c2e190b602be486984748e1bf20c
Italo Alessandro <italo.silva@sptech.school>
2024-05-22 10:52:52 -0300

    Adicionando rotas para gerenciar usuario

--- site/src/routes/gerenciarUsuario.js
+++ site/src/routes/gerenciarUsuario.js
@@ -1,13 +1,24 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
 
 
-var gerenciarController = require(`../controllers/gerenciarController`);
+var gerenciarController = require("../controllers/gerenciarController");
 
 router.get('/listar/:idUsuario/:acesso', function (req, res) {
   gerenciarController.listarUsuarios(req, res)
 })
 
+router.post('/cadastrarUsuario', function (req, res) {
+  gerenciarController.cadastrarUsuario(req, res)
+})
+
+router.put('/editarUsuario', function (req, res) {
+  gerenciarController.editarUsuario(req, res)
+})
+
+router.delete('/excluirUsuario/:idUsuario', function (req, res) {
+  gerenciarController.excluirUsuario(req, res)
+})
 
 
-module.exports = router
+module.exports = router;
