bac0a401b064f5319da97e32944599582f733a35
Italo Alessandro <italo.silva@sptech.school>
2024-05-22 10:52:12 -0300

    Adicionando model para gerenciar usuario

--- site/src/models/gerenciarModel.js
+++ site/src/models/gerenciarModel.js
@@ -10,7 +10,35 @@ function listarUsuarios(idUsuario, acesso) {
   return database.executar(instrucaoSql)
 }
 
+function cadastrarUsuario (nome, email, senha, setor, empresa, cargo, permissao, permissao_total) {
+  instrucaoSql = ''
+  instrucaoSql = `INSERT INTO funcionario (nome_funcionario, email_funcionario, senha_acesso, fk_setor, fk_empresa, cargo_funcionario, acesso_plataforma, permissao_total)
+  VALUES ('${nome}', '${email}', '${senha}', ${setor}, ${empresa}, '${cargo}', ${permissao}, ${permissao_total});
+  `
+
+  console.log('Executando a instrução SQL: \n' + instrucaoSql)
+  return database.executar(instrucaoSql) 
+}
+
+  function editarUsuario(nome, email, senha, setor, empresa, cargo, permissao, permissao_total) {
+  instrucaoSql = ''
+  instrucaoSql = `UPDATE funcionario SET nome_funcionario = '${nome}', email_funcionario = '${email}', senha_acesso = '${senha}', fk_setor = ${setor}, fk_empresa = ${empresa}, cargo_funcionario = '${cargo}', acesso_plataforma = ${permissao}, permissao_total = ${permissao_total}
+  WHERE email_funcionario = '${email}';
+  `
+  console.log('Executando a instrução SQL: \n' + instrucaoSql)
+  return database.executar(instrucaoSql)
+}
+
+  function excluirUsuario(idUsuario) {
+  instrucaoSql = ''
+  instrucaoSql = `DELETE FROM funcionario WHERE funcionario_id = ${idUsuario};`
+  console.log('Executando a instrução SQL: \n' + instrucaoSql)
+  return database.executar(instrucaoSql)
+}
 
 module.exports = {
-    listarUsuarios
+    listarUsuarios,
+    cadastrarUsuario,
+    editarUsuario,
+    excluirUsuario
 }
