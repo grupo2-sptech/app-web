d0a727e51dff54fde457a2c0fe494b23d5d7911c
JonathanCarvalho039 <jonathan.carvalho@sptech.school>
2024-06-06 18:03:20 -0300

    Alterando IP

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -5,7 +5,7 @@ var sql = require('mssql')
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-  server: '52.3.240.72',
+  server: '44.213.9.204',
   database: 'hardware_security2',
   user: 'sa',
   password: 'urubu100',
